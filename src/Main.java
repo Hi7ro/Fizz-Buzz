da8f22c41602b68bc9e46366e199929f8d1c00fe
xv7ro <a.k_windsor@protonmail.com>
2023-01-01 20:35:46 +0100

    Fizzy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String[] kaChing = {"Ka", "Ching", "Ka-Ching"};
+        
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println(kaChing[2]);
+            }
+            else if (i % 5 == 0) {
+                System.out.println(kaChing[1]);
+            }
+            else if (i % 3 == 0) {
+                System.out.println(kaChing[0]);
+            } else System.out.println(i);
+        }
+
     }
+
 }
